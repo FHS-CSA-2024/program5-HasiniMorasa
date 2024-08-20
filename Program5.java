be91d90184d742bb2618ac2e00debe4fc4d2fc89
HasiniMorasa <hmorasa9@gmail.com>
2024-08-20 10:10:31 -0700

    Update Program5.java
    Working on Program 5

--- Program5.java
+++ Program5.java
@@ -1,9 +1,32 @@
 //import stuff here?
 
 //Your code here
+//Your code here
+public class Program5 {
+    
+   public static void main(String[] args){
+//declare variables//
+int miles1 = 286  ;
+int gallon1 = 9 ;
+int miles2 = 412;
+int gallons2 = 40;
+int miles3  = 361;
+int gallons3 = 18;
+int miles4 = 161;
+int gallons4 = 11;
+ 
+//declare milesperGallon and average(Calculate)//
+double milesperGallon1 = (miles1/gallon1); 
+double Royaeaverage = milesperGallon1;
+double milesperGallon2 = (miles2/gallons2);
+double milesperGallon3 = (miles3/gallons3);
+double milesperGallon4 = (miles4/gallons4);
 
-//Paste console output below:
-/*
+    
+//Output//
+System.out.println("The Mushroom Cup Prix Racer Average Miles/Per Gallons");   
+System.out.println("Royales " + Royaeaverage + " m/g");
+
 
 
 */
