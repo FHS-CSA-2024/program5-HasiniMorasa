62acffa7b73699d32ab1f18707f6c5fe83365b04
HasiniMorasa <hmorasa9@gmail.com>
2024-08-21 19:48:37 -0700

    Finished Program5.java
    Finished Program 5. Learned how to use Math.round

--- Program5.java
+++ Program5.java
@@ -16,17 +16,23 @@ int miles4 = 161;
 int gallons4 = 11;
  
 //declare milesperGallon and average(Calculate)//
-double milesperGallon1 = (miles1/gallon1); 
-double Royaeaverage = milesperGallon1;
-double milesperGallon2 = (miles2/gallons2);
-double milesperGallon3 = (miles3/gallons3);
-double milesperGallon4 = (miles4/gallons4);
+double milesperGallon1 = (double)miles1/gallon1; 
+double Royaeaverage = (double) Math.round((milesperGallon1 ) * 10) /10;  // 31.8
+double milesperGallon2 = (double)miles2/gallons2;
+double Koopaverage = (milesperGallon2);
+double milesperGallon3 = (double) miles3/gallons3;
+double Pipeaverage = (double) Math.round((milesperGallon3) * 10) /10; // 20.1     
+double milesperGallon4 = (double)miles4/gallons4;
+double Wagonaverage = (double) Math.round((milesperGallon4) * 10) /10;      
+//rounding each output to the nearest tenth//      
 
-    
+
 //Output//
 System.out.println("The Mushroom Cup Prix Racer Average Miles/Per Gallons");   
-System.out.println("Royales " + Royaeaverage + " m/g");
+System.out.println("Royale averaged  " + Royaeaverage + " m/g");
+System.out.println("Koopa King averaged " + Koopaverage + " m/g");
+System.out.println("Piper Frame averaged " + Pipeaverage + " m/g");  
+System.out.println("Badwagon averaged " + Wagonaverage + " m/g");        
 
-
-
-*/
+  }
+}
